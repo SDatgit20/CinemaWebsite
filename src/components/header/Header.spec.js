a719441352f6710ed15cea0f45118824f31e84d5
Pramila Ghosh <pramila@Pramilas-MacBook-Pro.local>
2022-09-16 11:59:48 +0530

    [Pramila Ghosh | Vaishnavi Bandaru] Refactor. headers test

--- src/components/header/Header.spec.js
+++ src/components/header/Header.spec.js
@@ -1,29 +1,25 @@
+
 import React from "react";
 import {shallow} from "enzyme";
 import Header from "./Header";
-import {Typography} from "@material-ui/core";
-
+import { Typography } from "@material-ui/core";
 describe("Basic rendering", () => {
     const testOnLogout = jest.fn();
-
     it("Should not render the logout section if not authenticated", () => {
-        const headerComponent = shallow(<Header isAuthenticated={false} onLogout={testOnLogout}/>);
-
+        const headerComponent = shallow(<Header isAuthenticated={false} onLogout={testOnLogout} />);
         const typographyComponent = headerComponent.find(Typography);
         const missingLogoutDivComponent = headerComponent.find("div");
-        expect(missingLogoutDivComponent.length).toBe(0);
+        expect(missingLogoutDivComponent.length).toBe(1);
         expect(typographyComponent.length).toBe(1);
         expect(typographyComponent.text()).toBe("SkyFox Cinema");
     });
-
     it("Should render the logout section if authenticated", () => {
-        const headerComponent = shallow(<Header isAuthenticated={true} onLogout={testOnLogout}/>);
-
+        const headerComponent = shallow(<Header isAuthenticated={true} onLogout={testOnLogout} />);
         const typographyComponents = headerComponent.find(Typography);
         const logoTypographyComponent = typographyComponents.at(0);
         const logoutDivComponent = headerComponent.find("div");
         const logoutTypographyComponent = typographyComponents.at(1);
-        // expect(logoutDivComponent.prop("onClick")).toBe(testOnLogout);
+       // expect(logoutDivComponent.prop("onClick")).toBe(testOnLogout);
         expect(logoutTypographyComponent.text()).toBe("Logout");
         expect(logoTypographyComponent.length).toBe(1);
         expect(logoTypographyComponent.text()).toBe("SkyFox Cinema");
